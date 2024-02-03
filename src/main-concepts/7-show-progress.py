ddb4a536e5663660a1f81a0e4eb8ccd7aff8138d
Jonathan Zamora <jonzamora18@gmail.com>
2024-02-03 00:02:25 -0800

    add final example for main concepts

---
+++ src/main-concepts/7-show-progress.py
@@ -0,0 +1,21 @@
+"""
+My seventh app
+We now get into Showing Progress with a Progress Bar for Streamlit apps
+"""
+
+import streamlit as st
+import time
+
+st.write("Starting a long computation...")
+
+# Add a placeholder
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    # Update the progress bar with each iteration
+    latest_iteration.text(f"Iteration {i+1}")
+    bar.progress(i+1)
+    time.sleep(0.1)
+
+st.write("...and now we're done!")
